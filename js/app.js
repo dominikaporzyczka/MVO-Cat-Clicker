0a7951218cf9b6149918809e266440fdd550fd8b
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-04-12 13:12:17 +0200

    Add model object

---
+++ js/app.js
@@ -0,0 +1,10 @@
+(function () {
+
+    const model = {
+        cats: [
+            { name: 'Cat', imgSrc: 'img/cat1.jpg', clickCount: 0 },
+            { name: 'Fluffy', imgSrc: 'img/cat2.jpg', clickCount: 0 },
+            { name: 'Purr', imgSrc: 'img/cat3.jpg', clickCount: 0 },
+        ]
+    }
+})();
