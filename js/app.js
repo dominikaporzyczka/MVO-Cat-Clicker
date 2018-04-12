955b2946da84399783a168e967a7f8244e199107
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-04-12 13:43:20 +0200

    Add catListView functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 (function () {
 
     const model = {
+        currentCat: null,
         cats: [
             { name: 'Cat', imgSrc: 'img/cat1.jpg', clickCount: 0 },
             { name: 'Fluffy', imgSrc: 'img/cat2.jpg', clickCount: 0 },
@@ -13,8 +14,45 @@
             return model.cats;
         },
 
-        init: {
+        setCurrentCat: function (cat) {
+            model.currentCat = cat;
+        },
 
+        init: function () {
+            currentCat = model.cats[0],
+
+            catListView.init();
         }
     }
+
+    const catListView = {
+        init: function () {
+            this.catsList = document.querySelector('#cats_list');
+
+            this.render();
+            console.log(this);
+        },
+
+        render: function () {
+            const cats = octopus.getCats();
+            const elem = document.createDocumentFragment();
+
+            for (let i = 0; i < cats.length; i++) {
+                const cat = cats[i],
+                    catElem = document.createElement('li');
+
+                catElem.classList.add('cats_list__cat');
+                catElem.textContent = cat.name;
+                elem.appendChild(catElem);
+
+                catElem.addEventListener('click', function () {
+                    octopus.setCurrentCat(cat);
+                });  
+            }
+
+            this.catsList.appendChild(elem);
+        }
+    }
+
+    octopus.init();
 })();
